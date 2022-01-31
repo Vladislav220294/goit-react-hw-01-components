4a8d696541862f5eb4ba189fb153cf2c21a0de3b
Vladislav220294 <vladislav22021994@gmail.com>
2022-02-01 02:12:59 +0300

    добавляет третью разметку

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,13 +1,23 @@
 import PropTipes from 'prop-types';
-import FriendListItem from '../FriendList/FriendListItem'
+import FriendListItem from '../FriendList/FriendListItem';
+import s from './FriendList.module.css';
 
-const FriendList = ({friends}) => {
-    return (<ul class="friend-list"> {friends.map(el => <FriendListItem key={el.id }avatar={el.avatar} name={ el.name} isOnline={ el.isOnline} />)}
-  
-</ul> );
-}
- FriendList.PropTipes = {
-     friends: PropTipes.arrayOf(PropTipes.object).isRequired,
-     id: PropTipes.string.isRequired,
-    }
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={s.friendList}>
+      {friends.map(el => (
+        <FriendListItem
+          key={el.id}
+          avatar={el.avatar}
+          name={el.name}
+          isOnline={el.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+FriendList.PropTipes = {
+  friends: PropTipes.arrayOf(PropTipes.object).isRequired,
+  id: PropTipes.string.isRequired,
+};
 export default FriendList;
--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,16 +1,25 @@
 import PropTipes from 'prop-types';
+import s from './FriendlListItem.module.css';
 
-const FriendListItem = ({ avatar, name, isOnline}) => {
-    return ( <li >
-  <span class="status" style={isOnline === true ? {backgroundColor:  "green"  } : {backgroundColor:  "red"  } } ></span>
-  <img class="avatar" src={avatar} alt="User avatar" width="48" />
-        <p class="name">{ name}</p>
-</li> );
-}
- FriendListItem.PropTipes = {
-     avatar: PropTipes.string.isRequired,
-     name: PropTipes.string.isRequired,
-     isOnline: PropTipes.bool.isRequired,
-     
-    }
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={s.item}>
+      <span
+        className={s.status}
+        style={
+          isOnline === true
+            ? { backgroundColor: 'green' }
+            : { backgroundColor: 'red' }
+        }
+      ></span>
+      <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={s.name}>{name}</p>
+    </li>
+  );
+};
+FriendListItem.PropTipes = {
+  avatar: PropTipes.string.isRequired,
+  name: PropTipes.string.isRequired,
+  isOnline: PropTipes.bool.isRequired,
+};
 export default FriendListItem;
